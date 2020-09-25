051d95c338d75a849ef1a3a891424b2472f1cb46
mohammad.shafiuddin <mohammad.shafiuddin@ekino.com>
2020-09-25 19:44:40 +0530

    fix:css

--- src/component/CardTwo/cardTwo.js
+++ src/component/CardTwo/cardTwo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './CardTwo.scss'
+import './cardTwo.scss'
 import {Col, Container, Row} from "react-bootstrap";
 import Card from 'react-bootstrap/Card'
 import Link from "next/link";
