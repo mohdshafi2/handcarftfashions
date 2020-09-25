3a6634127a3f6e704f8aa62ec34dae2d760c744b
mohammad.shafiuddin <mohammad.shafiuddin@ekino.com>
2020-09-25 18:57:12 +0530

    fix:hand package

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Home = (props) => {
       <div className="intro">
         <div className="container">
           <div className="intro_txt">
-            <h1>Welcome HandCarft Product</h1>
+            <h1>Welcome Farmer Product</h1>
             <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
           </div>
         </div>
